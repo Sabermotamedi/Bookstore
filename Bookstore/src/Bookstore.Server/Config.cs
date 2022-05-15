52424dd3a664bded13036aff9c798e5c645cbf24
s.motamedi <s.motamedi@rayanbourse.ir>
2022-05-15 23:05:40 +0430

    Add

---
+++ Bookstore/src/Bookstore.Server/Config.cs
@@ -0,0 +1,62 @@
+﻿
+using IdentityServer4.Models;
+
+namespace Bookstore.Server
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+           new[]
+           {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> { "role" }
+                }
+           };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new[] { new ApiScope("BookstoreAPI.read"), new ApiScope("BookstoreAPI.write"), };
+        public static IEnumerable<ApiResource> ApiResources =>
+            new[]
+            {
+                new ApiResource("BookstoreAPI")
+                {
+                    Scopes = new List<string> { "BookstoreAPI.read", "BookstoreAPI.write" },
+                    ApiSecrets = new List<Secret> { new Secret("ScopeSecret".Sha256()) },
+                    UserClaims = new List<string> { "role" }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
+                    AllowedScopes = { "BookstoreAPI.read", "BookstoreAPI.write" }
+                },
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("ClientSecret1".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:5444/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:5444/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:5444/signout-callback-oidc" },
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "BookstoreAPI.read" },
+                    RequirePkce = true,
+                    RequireConsent = true,
+                    AllowPlainTextPkce = false
+                },
+            };
+    }
+}
--- Bookstore/src/Bookstore.Server/Program.cs
+++ Bookstore/src/Bookstore.Server/Program.cs
@@ -1,14 +1,34 @@
+using Bookstore.Server;
+using Bookstore.Server.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
+
+var seed = args.Contains("/seed");
+if (seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly.GetName().Name;
 var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+if (seed)
+{
+    SeedData.EnsureSeedData(defaultConnString);
+}
 
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseSqlServer(defaultConnString,
+        b => b.MigrationsAssembly(assembly)));
 
-builder.Services.AddIdentityServer()    
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
         options.ConfigureDbContext = b =>
@@ -29,6 +49,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+
 app.UseIdentityServer();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -40,7 +63,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
+app.UseEndpoints(endpoints => { 
+    endpoints.MapDefaultControllerRoute(); 
+});
+//app.MapControllers();
 
 app.Run();
